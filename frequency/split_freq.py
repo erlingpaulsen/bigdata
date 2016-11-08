e560f0043b16a06792e6a360f612af30e54b98f2
erlingpaulsen <erling.singstad.paulsen@gmail.com>
2016-11-08 11:43:22 +0100

    Split freq

--- frequency/split_freq.py
+++ frequency/split_freq.py
@@ -5,15 +5,15 @@ Created on Mon Nov 07 12:33:07 2016
 @author: ERLING
 """
 
-import pandas as pd
-import numpy as np 
-import timeit
+#import pandas as pd
+#import numpy as np 
+#import timeit
 
 # To import files from another folder
 import sys
 sys.path.append('../')
-import filehandler as fh
-import datahandler as dh
+#import filehandler as fh
+#import datahandler as dh
 import datastruct as ds
 from datastruct import Data
 
@@ -24,29 +24,30 @@ rounded_path = '../data/struct_rounded/'
 #freq_path = '../data/struct_freq_only/'
 ex = 'txt'
 
-files = fh.filesInDir(rounded_path,ex)
-n = len(files)
-print 'Number of files: '+str(n)
-
-# Loading structs
-starttime = timeit.default_timer()
-
-structs = []
-for f in files:
-    structs.append(ds.Data.load(rounded_path+f))
-    
-stoptime = timeit.default_timer()
-print 'Used '+ str(int(stoptime-starttime))+' seconds to load all structs.'
-
-# Concatenating structs
-starttime = timeit.default_timer()
-
-struct_full = ds.concatStructs(structs)
-
-stoptime = timeit.default_timer()
-print 'Used '+ str(int(stoptime-starttime))+' seconds to concatenate all structs.'
+#files = fh.filesInDir(rounded_path,ex)
+#n = len(files)
+#print 'Number of files: '+str(n)
+#
+## Loading structs
+#starttime = timeit.default_timer()
+#
+#structs = []
+#for f in files:
+#    structs.append(ds.Data.load(rounded_path+f))
+#    
+#stoptime = timeit.default_timer()
+#print 'Used '+ str(int(stoptime-starttime))+' seconds to load all structs.'
+#
+## Concatenating structs
+#starttime = timeit.default_timer()
+#
+#struct_full = ds.concatStructs(structs)
+#
+#stoptime = timeit.default_timer()
+#print 'Used '+ str(int(stoptime-starttime))+' seconds to concatenate all structs.'
 
 # Saving struct
-struct_full.save('FULL_PERIOD_ROUNDED.txt')
+struct_full = Data.load('../data/FULL_PERIOD_ROUNDED.txt')
+df_full = ds.toTable(struct_full)
 
 #struct_full = Data.load('FULL_PERIOD_ROUNDED.txt')
